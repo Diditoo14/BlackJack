ee3539daaf1bdd77ee7356fa22d5c95c1b296b5c
Diditoo14 <diditoo14@abv.bg>
2021-09-04 10:56:11 +0200

    create project

---
+++ BlackJack/BlackJack/BlackJack.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int arrayOfCards[52];
+
+int hCard1;
+int hCard2;
+
+int pCard1;
+int pCard2;
+
+int hCardSum;
+int pCardSum;
+
+int playerBet;
+char playerAnswer;
+
+int main()
+{
+	for (int i = 2; i <= 9; i++) 
+	{
+
+	}
+}
